c85fe5cba9a20b610fe09811776446aeab0c8057
Mariskt <mariannes@skillup.tech>
2021-01-13 11:13:43 -0800

    Newfiles

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6GyKYy4bCDh":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+alert("Hello!");
+}
+
